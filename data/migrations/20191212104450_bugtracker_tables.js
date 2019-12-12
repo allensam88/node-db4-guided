89fa75dfe2f5419cecce4f2f10de4382edae325d
allensam88 <54912919+allensam88@users.noreply.github.com>
2019-12-12 11:59:38 -0800

    made minor change to migration file foreign keys

--- data/migrations/20191212104450_bugtracker_tables.js
+++ data/migrations/20191212104450_bugtracker_tables.js
@@ -16,7 +16,7 @@ exports.up = function (knex) {
                 .notNullable()
                 .references('id')
                 .inTable('roles')
-                .onDelete('CASCADE')
+                .onDelete('CASCADE') // CASCADE, RESTRICT, DO NOTHING, SET NULL
                 .onUpdate('CASCADE');
         })
         .createTable('tickets', tbl => {
